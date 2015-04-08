982e4c42e4daf2872f5f93b49e5650e048a5bd74
luktt <luk.tt@hotmail.com>
2015-04-08 21:20:35 +0100

    program added

---
+++ naukaCplotek2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naukaCplotek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Witaj swiecie2!");
+            Console.Read();
+        }
+    }
+}
